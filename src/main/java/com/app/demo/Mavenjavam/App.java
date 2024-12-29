ca621189df65f87e332655b4981009e117a23161
sreeja <sameeraeeda@gmail.com>
2024-12-29 21:22:50 +0530

    javamavensample

---
+++ src/main/java/com/app/demo/Mavenjavam/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Mavenjavam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
